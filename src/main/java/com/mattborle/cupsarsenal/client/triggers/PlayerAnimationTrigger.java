93875a4af4d531c72b96c4c24fc28b00fd26f480
Matt Borle (Cup) <mattrb@telus.net>
2022-12-14 20:51:23 -0800

    Mixin exists, but not sure what to do next.

--- src/main/java/com/mattborle/cupsarsenal/client/triggers/PlayerAnimationTrigger.java
+++ src/main/java/com/mattborle/cupsarsenal/client/triggers/PlayerAnimationTrigger.java
@@ -6,6 +6,7 @@ import dev.kosmx.playerAnim.api.layered.IAnimation;
 import dev.kosmx.playerAnim.api.layered.KeyframeAnimationPlayer;
 import dev.kosmx.playerAnim.api.layered.ModifierLayer;
 import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationAccess;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationFactory;
 import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
@@ -14,8 +15,12 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+
+import java.util.Objects;
 
 
 /**
@@ -28,29 +33,21 @@ public class PlayerAnimationTrigger {
 
     @SubscribeEvent
     public void onHeldItemChanged(TickEvent.PlayerTickEvent event){//TODO: of course this is bad, we will need a mixin to detect changed held item
-        CupsArsenal.LOGGER.debug("1");
         var player = Minecraft.getInstance().player;
 
         if (player == null) return;
-        CupsArsenal.LOGGER.debug("2");
-        if(Minecraft.getInstance().player.getInventory().getSelected() == null) return;
-        CupsArsenal.LOGGER.debug("3");
 
-        heldItem = Minecraft.getInstance().player.getInventory().getSelected().getItem();
-        CupsArsenal.LOGGER.debug("4");
+        heldItem = Minecraft.getInstance().player.getMainHandItem().getItem();
         // Get the animations for the player, if this fails, print an error and return without crashing.
         var animation = (ModifierLayer<IAnimation>) PlayerAnimationAccess.getPlayerAssociatedData((AbstractClientPlayer) player).get(new ResourceLocation(CupsArsenal.MOD_ID, "animation"));
         if (animation == null) {
             CupsArsenal.LOGGER.error("Cup's Arsenal - Missing player animation reference @ PlayerAnimationTrigger.java. Please report this and what you did before this error was logged.");
             return;
         }
-        CupsArsenal.LOGGER.debug("5");
         // Animation selection:
         if (heldItem == ItemRegistry.MAGMA_SPEWER.get())
         {
-            animation.setAnimation(new KeyframeAnimationPlayer(PlayerAnimationRegistry.getAnimation(new ResourceLocation("cupsarsenal", "animation.model.gun.large.low.idle"))));
-            CupsArsenal.LOGGER.debug("6");
+            animation.setAnimation(new KeyframeAnimationPlayer(Objects.requireNonNull(PlayerAnimationRegistry.getAnimation(new ResourceLocation("cupsarsenal", "animation.model.gun.large.low.idle")))));
         }
-        CupsArsenal.LOGGER.debug("7");
     }
 }
---
+++ src/main/java/com/mattborle/cupsarsenal/mixin/MixinHeldItem.java
@@ -0,0 +1,14 @@
+package com.mattborle.cupsarsenal.mixin;
+
+import com.mattborle.cupsarsenal.CupsArsenal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.eventbus.api.Event;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+@Mixin(Player.class)
+public class MixinHeldItem {
+
+}
